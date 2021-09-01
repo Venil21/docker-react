bf9d707b2bdd25798bb337f7d6a4287f6d53599a
bkvenilreddy <bkvenilreddy@gmail.com>
2021-09-01 16:59:50 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Helloo Namaste
+          Helloo Namaste People
         </p>
         <a
           className="App-link"
